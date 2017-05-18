c31222d81f884f818b9320f1160ddbcca2e5b41d
Viktor Gorbelik <gorvikvik@mail.ru>
2017-05-19 00:36:55 +0400

    Improve the application_controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 def hello
-    render html: "Preved, Medved!"
+    render html: "hello, world!"
   end
 end
